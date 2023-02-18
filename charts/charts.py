0dcc38dd1ef3aa7e0fd02d6662f640107d40be46
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-17 19:07:57 -0600

    Figure created with python code

--- charts/charts.py
+++ charts/charts.py
@@ -4,12 +4,14 @@ def generatePieChart(labels, values):
 	fig, ax = plt.subplots()
 	ax.pie(values, labels=labels)
 	ax.axis("equal")
-	plt.show()
+	plt.savefig("./figures/pie.png")
+	plt.close()
 
 def generateBarChart(labels, values):
 	fig, ax = plt.subplots()
 	ax.bar(labels, values)
-	plt.show()
+	plt.savefig("./figures/bar.png")
+	plt.close()
 
 if __name__ == "__main__":
 	generatePieChart([10, 12, 8], ["a", "b", "c"])
