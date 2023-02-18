a0d2d32f0d2498f871421f33e1bfd05bb2cff92e
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-18 12:37:49 -0600

    Dynamic country to show done

--- app/main.py
+++ app/main.py
@@ -8,24 +8,29 @@ filename = "./app/data.csv"
 content = read.readFile(filename)
 
 # Chart global population
+'''
 countryNames = modify("Country/Territory", content)
 countryPop = modify("2022 Population", content)
 
 charts.generatePieChart(countryNames, countryPop)
+'''
 
 
 # Chart one country population over time
-'''
-chosenCountry = retrieve.getCountry("Country/Territory", "Mexico", content)
+country = ""
+
+while not country or not list( filter(lambda ctry: ctry["Country/Territory"] == country.capitalize(), content) ):
+	country = input("Choose a country: ")
+
+chosenCountry = retrieve.getCountry("Country/Territory", country.capitalize(), content)
 countryPops = retrieve.getPopulations(chosenCountry)
 
 labels = []
 values = []
 
 for pop in countryPops:
-  item = list( pop.items() )[0]
-  labels.append(item[0])
-  values.append(int(item[1]))
+	item = list( pop.items() )[0]
+	labels.append(item[0])
+	values.append(int(item[1]))
 
 charts.generateBarChart(labels, values)
-'''
